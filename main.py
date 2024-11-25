22cced45a2c775c0e2de70f3205c9904f6510d81
Choi Changgyu <cck981007@gmail.com>
2024-11-25 22:13:50 +0900

    tts add

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import streamlit as st
+import speech_recognition as sr
 from streamlit_chat import message
 from openai import OpenAI
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import pyttsx3
 import os
 import json
 
 load_dotenv()
 client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 now_dir = os.getcwd()
-
-print()
-print()
-print()
-print()
-
 
 st.title("Chat bot test")
 
@@ -29,8 +25,8 @@ def init():
         st.session_state["active"] = ""
     if "side_data" not in st.session_state: # 사이드바에 표시하기위한 데이터
         st.session_state["side_data"] = []
-    if "button_clicked" not in st.session_state:    # 동적 추가 버튼 클릭 여부 
-        st.session_state["button_clicked"] = False
+    if "transcript" not in st.session_state: # 음성 번역
+        st.session_state["transcript"] = ""
 
     # 대화내역 불러오기 위한 데이터 초기화
     if os.path.isdir(now_dir + "\History"):
@@ -55,9 +51,7 @@ with st.sidebar:
         for room_name, file_name in room.items():
             button_key = f"{room_name}_{file_name}{i}"  
             # 선택된 버튼 다른 타입으로 표시 (보류)
-            type = "primary" if st.session_state["active"] == now_dir + "/History/" + file_name else "secondary"
-
-            if st.button(room_name, key=button_key, type=type):  # 각 대화방 이름을 버튼으로 출력
+            if st.button(room_name, key=button_key):  # 각 대화방 이름을 버튼으로 출력
                 st.session_state["messages"] = []
                 st.session_state["active"] = now_dir + "/History/" + file_name
                 with placeholder.container():
@@ -65,6 +59,8 @@ with st.sidebar:
                         json_data = json.load(f)
                         for message in json_data:
                             st.session_state.messages.append({"role":message["role"], "content":message["content"]})
+                            with st.chat_message(message["role"]):
+                                st.write(message["content"])
                  
 # 채팅 내역 session_state 저장
 def session_save(data):
@@ -80,24 +76,20 @@ def session_save(data):
         active = st.session_state.active
         # 첫 질문 - active 값 없음 - dump로 생성
         if active == "":
-            print("file", history_dir + file_name)
             with open(history_dir + file_name, 'w', encoding='UTF8') as f:
                 json.dump([data], f)
 
                 room_name = data["content"][0:20]
                 st.session_state["active"] = f.name
                 st.session_state.side_data.insert(0,{room_name:file_name})
-
+                
                 with sidebar_placeholder.container():
                     button_key = f"{room_name}_{file_name}{len(st.session_state.side_data)}"  
                     # 동적으로 버튼 추가시 - 클릭이벤트 비정상 작동 - (보류)
                     if st.button(room_name, key=button_key):  # 각 대화방 이름을 버튼으로 출력
-                        st.session_state["button_clicked"] = True
-                        # st.session_state["messages"] = []
-                        st.session_state["active"] = now_dir + "/History/" + file_name
+                        print("new btn")
         # 두번째는 - session_state 값 있음 - update
         elif os.path.isfile(active):
-            print("file", active)
             with open(active, 'r', encoding='UTF8') as f:
                 try:
                     # 기존 대화 불러오기
@@ -110,36 +102,45 @@ def session_save(data):
                 json_data.append(data)
             with open(active, 'w', encoding='UTF8') as f:
                 json.dump(json_data, f)
-
-if st.session_state["button_clicked"]:
-    print("new")
-    placeholder.empty()
-    with placeholder.container():
-        with open(os.path.join(now_dir, "History", file_name), 'r', encoding='UTF8') as f:
-            json_data = json.load(f)
-            for message in json_data:
-                with st.chat_message(message["role"]):
-                    st.write(message["content"])
     
-# 메세지 추가시 화면에 표시
-for message in st.session_state["messages"]:
-    print("mesg", message)
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
 
+# 음성 입력을 위한 함수
+def get_audio_input():
+    r = sr.Recognizer()
 
-# 입력시 입력값과 결과값 st.session_state["messages"]에 추가
-if prompt := st.chat_input("Say something"):
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+
+    # 구글 웹 음성 API로 인식하기 
+    try:
+        print("Google Speech : " + r.recognize_google(audio, language='ko'))
+        return r.recognize_google(audio, language='ko')
+    except sr.UnknownValueError as e:
+        print("Google Speech ".format(e))
+        return None
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        return None
+
+def text_to_speech(text):       # TTS 
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def chatbot(prompt, isVoice):
+    # 기본 메시지 화면에 표시
+    for message in st.session_state["messages"]:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
     data = {"role":"user", "content":prompt}
     st.session_state.messages.append(data)
-
     session_save(data)
-    print(data)
 
-    with st.chat_message("user"):
+    with st.chat_message("user"):  # 사용자 채팅 표시
         st.write(prompt)
     
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant"):      # 답변 채팅 표시 - stream 실시간 채팅
         stream = client.chat.completions.create(
             model = st.session_state["openai_model"],
             messages = [
@@ -154,4 +155,17 @@ if prompt := st.chat_input("Say something"):
         st.session_state.messages.append({"role":"assistant", "content":response})
         
         session_save(data)
-        print(response)
+        
+        if isVoice:     # isVoice 파라미터에 따라 읽기
+            text_to_speech(response)
+
+if st.button("마이크"):             # 마이크 입력시 보이스 재생
+    user_input = get_audio_input()
+    if user_input is not None:
+        text_to_speech(user_input)
+        chatbot(user_input, True)
+
+if prompt := st.chat_input("Say something"):        # 채팅 입력시
+    chatbot(prompt, False)
+
+
